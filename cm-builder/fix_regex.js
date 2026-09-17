e986e276bc24485a8f5e132e45ead38213a933cd
HayaMaria <89562314+HayaMaria@users.noreply.github.com>
2026-09-17 17:50:03 +0300

    Отредактировала текст в подсказках и настройках.

--- cm-builder/fix_regex.js
+++
@@ -1,4 +0,0 @@
-var C=String.fromCharCode; 
-var Q=C(39);var B=C(92);var D=C(36);var A=C(38); 
-var fs=require('fs');var src=fs.readFileSync('src/editor.js','utf8'); 
-var si=src.indexOf('function smartToggleFormat'); 
